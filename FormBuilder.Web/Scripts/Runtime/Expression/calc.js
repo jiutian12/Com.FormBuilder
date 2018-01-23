8bfc8e4879f16fc4ce3d12450c2f8c7dc64862cc
skiphyuk <liweilong0921@126.com>
2018-01-23 15:38:23 +0800

    341234

---
+++ FormBuilder.Web/Scripts/Runtime/Expression/calc.js
@@ -0,0 +1,78 @@
+﻿window.Page.Calc = (function (parser, model, ui, win, $) {
+
+    var CalcManger = function () {
+
+    }
+
+    var HashExpData = {
+        //"input_j8n3pm7z0d9rqe15um": { exp: "'{Data:PurCat.CODE}'+'{Data:PurCat.NAME}'" }
+    };
+    var HashExpMap = {
+        //"PurCat.CODE": [
+        //    { ctrlid: "input_j8n3pm7z0d9rqe15um" }
+        //],
+        //"PurCat.NAME": [
+        //    { ctrlid: "input_j8n3pm7z0d9rqe15um" }
+        //]
+    };
+
+
+
+    CalcManger.prototype = {
+        init: function (data) {
+            $.each(data, function (key, obj) {
+                var dep = obj.dep;
+                $.each(dep, function (i, value) {
+                    HashExpMap[value] = HashExpMap[value] || [];
+                    HashExpMap[value].push({
+                        ctrlid: key
+                    });
+                });
+            });
+            HashExpData = data;
+        },
+
+        triggerGrid: function () {
+            // 界面触发时机
+            // 新增行 删除行 触发所有字段
+            // 单元格值改变
+            // 帮助回弹 多选
+        },
+        getMainValue: function (field) {
+            return model.getMainObjectValue(field);
+        },
+        getGridValue: function (rowdata, index, table, column) {
+            return rowdata[column];
+            //
+        },
+        trigger: function (table, field, gridid, index, rowdata) {
+            var self = this;
+
+            if (!HashExpMap[table + "." + field]) return;
+            if (!gridid) {
+                // 先处理主表内容
+                var arr = HashExpMap[table + "." + field];
+                for (var item in arr) {
+                    var exp = HashExpData[arr[item].ctrlid].express;// 提前编译好？
+                    exp = parser.parser(exp);
+                    var result = eval(exp);
+                    $("#" + arr[item].ctrlid).leeUI().setValue(result); //循环触发？
+                }
+            } else {
+                var arr = HashExpMap[table + "." + field];
+
+                for (var item in arr) {
+                    var exp = HashExpData[arr[item].ctrlid].express;// 提前编译好？
+                    exp = parser.parser(exp);
+                    var result = eval(exp);
+                    var field = HashExpData[arr[item].ctrlid].field;
+                    $("#" + gridid).leeUI().updateCell(field, result, index);
+                    //alert(result);
+                }
+            }
+        }
+    };
+
+
+    return new CalcManger();
+})(Page.ExpressParser, Page.Model, Page.UI, window, $);
---
+++ FormBuilder.Web/Scripts/Runtime/Expression/parser.js
@@ -0,0 +1,120 @@
+﻿window.Page.ExpressParser = (function (parser, model, ui, win, $) {
+
+    var ParserManger = function () {
+
+    }
+
+    var variableParserRegisterOptions = [{
+        type: "session", // session
+        matchExpression: /\{Session.(\w+)\}/,
+        parser: "sessionParser"
+    }, {
+        type: "formState", // 界面状态
+        matchExpression: /\{FormState.(\w+)\}/,
+        parser: "formStateParser"
+    }, {
+        type: "data",// 数据模型
+        matchExpression: /\{Data:(\w+).(\w+)\}/,
+        parser: "dataParser"
+    }
+    ]; 
+    ParserManger.prototype = {
+        init: function () {
+        },
+        getParser: function (express) {
+            var self = this;
+            var matchedParser;
+            $.each(variableParserRegisterOptions, function (i, parser) {
+                matchResult = express.match(parser.matchExpression);
+                if (matchResult && matchResult.length) {
+                    matchedParser = self[parser.parser];
+                }
+            });
+            return matchedParser;
+        },
+        parser: function (express) {
+            var parser = this.getParser(express);
+            if (parser) {
+                return parser(express);
+            }
+            return express;
+        },
+        sessionParser: function (express) {
+            var matchExpression = /\{Session.(\w+)\}/;
+            var matchKeys, sessionKey, parsedResult = express;
+            if (typeof express === "string") {
+                matchKeys = express.match(matchExpression);
+                if (matchKeys && matchKeys.length === 2) {
+                    sessionKey = matchKeys[1];
+                    value = Page.Context.get(sessionKey);
+                    if (value === undefined || value === null) {
+                        value = '';
+                    }
+                    parsedResult = express.replace(matchExpression, value);// 替换表达式的数据
+                }
+            }
+            return parsedResult;
+        },
+
+        formStateParser: function (express) {
+            var matchExpression = /\{FormState.(\w+)\}/;
+            var matchKeys, formKey, parsedResult = express;
+            if (typeof express === "string") {
+                matchKeys = express.match(matchExpression);
+                if (matchKeys && matchKeys.length === 2) {
+                    formKey = matchKeys[1];
+                    value = Page.FormSate.get(formKey);
+                    if (value === undefined || value === null) {
+                        value = '';
+                    }
+                    parsedResult = express.replace(matchExpression, value);// 替换表达式的数据
+                }
+            }
+            return parsedResult;
+        },
+
+
+        dataParser: function (express) {
+            var matchExpression = /\{Data:(\w+).(\w+)\}/;
+            var matchKeys, sessionKey, parsedResult = express;
+            if (typeof express === "string") {
+                matchKeys = express.match(matchExpression);
+                while (matchKeys && matchKeys.length === 3) {
+                    sessionKey = matchKeys[1];
+                    value = model.getMainObjectValue(matchKeys[2]);
+
+                    if (value === undefined || value === null) {
+                        value = '';
+                    }
+                    express = express.replace(matchExpression, value);// 替换表达式的数据
+                    matchKeys = express.match(matchExpression);
+                }
+            }
+            return express;
+        },
+        eval: function (express) {
+            // 动态计算表达式 支持复杂js语法
+
+            var self = this;
+            var matchedParser;
+            $.each(variableParserRegisterOptions, function (i, parser) {
+                matchResult = express.match(parser.matchExpression);
+                if (matchResult && matchResult.length) {
+                    matchedParser = self[parser.parser];
+                    express = matchedParser(express);// 处理表达式
+                }
+            });
+
+
+            if (express.indexOf("return") == 0) {
+                var func = new Function(express);
+                return func.call(window, []);
+            } else {
+                return eval(express);
+            }
+        }
+    };
+
+    parser = new ParserManger();
+    return parser;
+})(Page.ExpressParser, Page.Model, Page.UI, window, $);
