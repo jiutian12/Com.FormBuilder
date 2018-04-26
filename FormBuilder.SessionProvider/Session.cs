6114c5c3f12776deddab3cbd793a2cea07ba8e05
skiphyuk <liweilong0921@126.com>
2018-04-26 11:07:30 +0800

    修改增加新工程

--- FormBuilder.SessionProvider/Session.cs
+++ FormBuilder.SessionProvider/Session.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FormBuilder.Utilities;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,92 @@ using System.Threading.Tasks;
 
 namespace FormBuilder.SessionProvider
 {
-    class Session
+    public class Session : ISessionProvider
     {
+        private string LoginUserKey = "FormBuilder";
+
+        private string LoginTokenKey = "FormBuilderSid";
+        public void AddCurrent(ISessionKey user)
+        {
+            // 登录成功写入cookie 写入JWTtoken？
+            CookieHelper.WriteCookie(LoginUserKey, DESEncrypt.Encrypt(JsonConvert.SerializeObject(user)));
+            CookieHelper.WriteCookie(LoginTokenKey, user.UserID);
+            // 数据库的话记录   tokenid
+        }
+
+        public virtual ISessionKey Current()
+        {
+            try
+            {
+                // 这里可以做线程缓存处理 ？这里需要check校验？ 分两步 第一步bulid 然后校验
+
+                // Session build -StateCheck
+                ISessionKey user = new ISessionKey();
+                user = JsonConvert.DeserializeObject<ISessionKey>(DESEncrypt.Decrypt(CookieHelper.GetCookie(LoginUserKey)));
+                return user;
+            }
+            catch
+            {
+                //return new ISessionKey();
+                throw new Exception("登录信息超时，请重新登录。");
+            }
+        }
+
+
+        public void EmptyCurrent()
+        {
+            // 清除cookie
+            CookieHelper.DelCookie(LoginUserKey);
+        }
+
+
+
+        /// <summary>
+        /// 是否登录超时
+        /// </summary>
+        /// <returns></returns>
+        public bool IsOverdue()
+        {
+            var session = Current();
+            if (session == null)
+                return true;
+            else
+                return false;
+        }
+
+
+
+
+        /// <summary>
+        /// buildSession信息
+        /// </summary>
+        private void buildSession(string stateCookie)
+        {
+            var parts = stateCookie.Split('.');
+            if (parts.Length != 3) throw new Exception("invalid Session Info!");
+            var payload = parts[1];
+            var payloadJson = Encoding.UTF8.GetString(Base64UrlDecode(payload));
+            var session = Newtonsoft.Json.JsonConvert.DeserializeObject<ISessionKey>(payloadJson);
+
+            //解析完成后存储在当前线程上下文
+
+
+        }
+
+        private static byte[] Base64UrlDecode(string input)
+        {
+            var output = input;
+            output = output.Replace('-', '+'); // 62nd char of encoding  
+            output = output.Replace('_', '/'); // 63rd char of encoding  
+            switch (output.Length % 4) // Pad with trailing '='s  
+            {
+                case 0: break; // No pad chars in this case  
+                case 2: output += "=="; break; // Two pad chars  
+                case 3: output += "="; break; // One pad char  
+                default: throw new System.Exception("Illegal base64url string!");
+            }
+            var converted = Convert.FromBase64String(output); // Standard base64 decoder  
+            return converted;
+        }
     }
 }
