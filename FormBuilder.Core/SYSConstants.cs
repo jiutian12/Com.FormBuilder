310bab469b1696ad95786d32b1f0db2c6c67c118
skiphyuk <liweilong0921@126.com>
2018-04-26 11:32:53 +0800

    增加静态变量

---
+++ FormBuilder.Core/SYSConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormBuilder.Core
+{
+    public class SYSConstants
+    {
+        public static string JWTSecret = "FB#X2018";
+        public static string LoginJWTKey = "FBJWT";
+        public static string LoginTokenKey = "FBJWT";
+        public static string LoginUserKey = "FBJWT";
+    }
+}
