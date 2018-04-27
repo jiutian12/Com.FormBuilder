3c5d8d2f3f465b6d85b9f30ce5992bb831e67eb6
skiphyuk <liweilong0921@126.com>
2018-04-27 09:00:29 +0800

    3

---
+++ FormBuilder.ExportTool/www/Scripts/app.js
@@ -0,0 +1,15 @@
+﻿window.JSBridge={
+    begin:function(total){
+        // 开始导出
+    },
+    append:function(mes,progress){
+        // 写入日志
+    },
+    done:function(){
+        //完成导出
+    }
+};
+
+
+
+ 
