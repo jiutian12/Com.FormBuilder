cf46636ae5c21d71fa6b9b6b7d279cb387c86afb
skiphyuk <liweilong0921@126.com>
2018-05-10 22:47:21 +0800

    修改相关目录结构

---
+++ FormBuilder.ADModel/FBOrgInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using NPoco;
+
+namespace FormBuilder.ADModel
+{
+
+    [TableName("FBOrgInfo")]
+    [PrimaryKey("ID", AutoIncrement = false)]
+    public class FBOrgInfo
+    {
+    }
+}
