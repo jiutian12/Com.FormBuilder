314f69c0d7c8dfd357b528f922a14e7114a0f433
skiphyuk <liweilong0921@126.com>
2018-05-12 15:13:51 +0800

    3

---
+++ FromBuilder.Service/Account/Menu/FBMenuService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FormBuilder.DataAccess.Interface;
+using FormBuilder.Model;
+using FormBuilder.Repository;
+using FormBuilder.Utilities;
+using NPoco;
+using FormBuilder.DataAccess;
+
+namespace FormBuilder.Service
+{
+    public class FBMenuService : Repository<FBMenuInfo>, IFBMenuService
+    {
+
+        #region ctr
+        public FBMenuService(IDbContext context) : base(context)
+        {
+
+
+        }
+        #endregion
+
+
+
+        public void deleteData(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<FBMenuInfo> getMenuList(string appcode, string keyword, string parentID, string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FBMenuInfo getModel(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GridViewModel<FBMenuInfo> getPageList(string type, string keyword, int currentPage, int perPage, out long totalPages, out long totalItems)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void saveData(FBMenuInfo model)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+    }
+}
---
+++ FromBuilder.Service/Account/Menu/IFBMenuService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FormBuilder.Model;
+using FormBuilder.Utilities;
+
+
+
+
+namespace FormBuilder.Service
+{
+    public interface IFBMenuService : IDisposable
+    {
+        void saveData(FBMenuInfo model);
+
+        void deleteData(string id);
+
+        FBMenuInfo getModel(string id);
+
+        GridViewModel<FBMenuInfo> getPageList(string type, string keyword, int currentPage, int perPage, out long totalPages, out long totalItems);
+
+        List<FBMenuInfo> getMenuList(string appcode, string keyword, string parentID, string filter);
+
+    }
+}
