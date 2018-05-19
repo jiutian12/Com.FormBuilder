dd21803d2159369baf9ee4d5e98c22990657eac5
skiphyuk <liweilong0921@126.com>
2018-05-19 22:49:06 +0800

    增加表单扩展验证

---
+++ FormBuilder.Test/FBMenuExtend.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using FormBuilder.Service;
+using NPoco;
+
+namespace FormBuilder.Test
+{
+    public class FBMenuExtend : IFBModelExtend
+    {
+        public string AfterDelete(string ModelID, string DataID, Database db)
+        {
+            return "";
+        }
+
+        public string AfterSave(string ModelID, DataSet ds, string Status, Database db)
+        {
+            return "";
+        }
+
+        public string BeforeDelete(string ModelID, string DataID, Database db)
+        {
+            return "";
+        }
+
+        public string BeforeSave(string ModelID, DataSet ds, string Status, Database db)
+        {
+            db.Execute("delete from FBLog");
+            return "";
+        }
+    }
+}
