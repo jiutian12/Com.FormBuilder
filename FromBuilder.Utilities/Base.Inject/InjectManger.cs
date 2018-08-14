2a966724b4127695c2741c9593626f8b8f14979c
skiphyuk <liweilong0921@126.com>
2018-08-14 15:19:46 +0800

    44

---
+++ FromBuilder.Utilities/Base.Inject/InjectManger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml;
+
+namespace FormBuilder.Utilities
+{
+    public class InjectManger
+    {
+        private static XmlNodeList nodelist;
+        static InjectManger()
+        {
+            try
+            {
+                FileInfo configFile = new FileInfo(HttpContext.Current.Server.MapPath("~/config/FormBuilder.config"));
+                var assemblyName = "";
+                var className = "";
+                XmlDocument xmlIAUConfig = new XmlDocument();
+                xmlIAUConfig.Load(HttpContext.Current.Server.MapPath("~/config/FormBuilder.config"));
+                 
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
