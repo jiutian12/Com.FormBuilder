6e6475caf5a4ed1ec3a1f37fac2155d5b11b01c5
skiphyuk <liweilong0921@126.com>
2018-10-28 22:13:55 +0800

    增加登录日志

---
+++ FromBuilder.Model/Admin/FBLoginLog.cs
@@ -0,0 +1,22 @@
+﻿using NPoco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormBuilder.Model
+{
+    [TableName("FBLoginLog")]
+    [PrimaryKey("ID", AutoIncrement = false)]
+    public class FBLoginLog
+    {
+        public string ID { get; set; }
+        public string UID { get; set; }
+        public string LoginIP { get; set; }
+        public string LoginMachine { get; set; }
+        public string LoginResult { get; set; }
+        public string LoginTime { get; set; }
+
+    }
+}
